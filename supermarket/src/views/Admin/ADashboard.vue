5a68c6207b2a4abfd13eae218361ab8aa059e15a
Ahmedbm99 <101886914+Ahmedbm99@users.noreply.github.com>
2023-06-06 16:41:07 +0100

    updated 06/06/2023::16:41

--- supermarket/src/views/Admin/ADashboard.vue
+++ supermarket/src/views/Admin/ADashboard.vue
@@ -7,11 +7,12 @@
           <h3>Hello {{ user.firstName }} {{ user.lastName }},</h3>
           <h5>Welcome to admin dashboard</h5>
         </b-col>
-        <b-col>
-          <img src="/img/svg/overview.svg" alt="" width="300" />
-        </b-col>
+       
       </b-row>
     </b-container>
+    <div>
+   <section id="container" style="height: 750px"/>
+  </div>
       <MyFooter class="mt-5" />
     </div>
   </template>
@@ -19,7 +20,7 @@
   <script>
   import ATopHeader from "@/components/Admins/ATopHeader.vue";
   import MyFooter from "@/components/Common/MyFooter.vue";
-  
+  import * as pbi from "powerbi-client";
 
   export default {
     name: "AOverview",
@@ -34,14 +35,29 @@
           
       };
     },
-    
-  };
-    
- 
-  
-  
+    mounted () {
+   const permissions = pbi.models.Permissions.All
+   const config = {
+     type: 'dashboard',
+     tokenType: pbi.models.TokenType.Embed,
+     accessToken:"true",
+     embedUrl: "https://app.powerbi.com/reportEmbed?reportId=2e182efc-2f2c-4fd1-a6b4-1a1a64b48726",
+     id: "dbd6664d-4eb9-46eb-99d8-5c43ba153c61",
+     pageView: 'fitToWidth',
+     permissions: permissions,
+   };
 
+   let powerbi = new pbi.service.Service(pbi.factories.hpmFactory, pbi.factories.wpmpFactory, pbi.factories.routerFactory);
+   const dashboardContainer = document.getElementById('container');
+   const dashboard = powerbi.embed(dashboardContainer, config);
 
+   dashboard.off("loaded");
+   dashboard.off("rendered");
+   dashboard.on("error", function () {
+     this.dashboard.off("error");
+   });
+  }
+ };
 
   </script>
   
